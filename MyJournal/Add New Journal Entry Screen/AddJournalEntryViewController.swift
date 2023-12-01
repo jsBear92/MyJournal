24fc345459490b7002c487bfafd7edd9f9a604a3
Jaeseong Jeong <js104427@gmail.com>
2023-12-01 13:35:04 +0800

    add update table view with a new journal entry

---
+++ MyJournal/Add New Journal Entry Screen/AddJournalEntryViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AddJournalEntryViewController.swift
+//  MyJournal
+//
+//  Created by Jaeseong Jeong on 1/12/2023.
+//
+
+import UIKit
+
+class AddJournalEntryViewController: UIViewController {
+    
+    // MARK: - Properties
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var bodyTextView: UITextView!
+    @IBOutlet var photoImageView: UIImageView!
+    var newJournalEntry: JournalEntry?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        let title = titleTextField.text ?? ""
+        let body = bodyTextView.text ?? ""
+        let photo = photoImageView.image
+        let rating = 3
+        newJournalEntry = JournalEntry(rating: rating, title: title, body: body, photo: photo)
+    }
+
+}
