dd8d3727748bf8a71e49daab78d26b9020ddd0dc
Jaeseong Jeong <js104427@gmail.com>
2023-12-02 17:30:39 +0800

    added feature connecting navigation and loading screen

--- MyJournal/Journal Entry Detail Screen/JournalEntryDetailViewcontroller.swift
+++ MyJournal/Journal Entry Detail Screen/JournalEntryDetailViewcontroller.swift
@@ -14,4 +14,29 @@ class JournalEntryDetailViewcontroller: UITableViewController {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var bodyTextView: UITextView!
     @IBOutlet var photoImageView: UIImageView!
+    var sampleJournalEntryData = SampleJournalEntryData()
+    var selectedJournalEntry: JournalEntry?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dateLabel.text = selectedJournalEntry?.date.formatted(
+            .dateTime.day().month(.wide).year()
+        )
+        titleLabel.text = selectedJournalEntry?.entryTitle
+        bodyTextView.text = selectedJournalEntry?.entryBody
+        photoImageView.image = selectedJournalEntry?.photo
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "entryDetail" else {
+            return
+        }
+        guard let journalEntryDetailViewController = segue.destination as? JournalEntryDetailViewcontroller, let selectedJournalEntryCell = sender as? JournalListTableViewCell, let indexPath = tableView.indexPath(for: selectedJournalEntryCell) else {
+            fatalError("Could not get indexPath")
+        }
+        let selectedJournalEntry = sampleJournalEntryData.journalEntries[indexPath.row]
+        journalEntryDetailViewController.selectedJournalEntry = selectedJournalEntry
+    }
 }
