44937ee5d71df8c2bcb8ffbdc6a4a5b9c0326e9d
Jaeseong Jeong <js104427@gmail.com>
2023-12-02 17:06:20 +0800

    added feature UI connected to Entry Detail screen

---
+++ MyJournal/Journal Entry Detail Screen/JournalEntryDetailViewcontroller.swift
@@ -0,0 +1,17 @@
+//
+//  JournalEntryDetailViewcontroller.swift
+//  MyJournal
+//
+//  Created by Jaeseong Jeong on 2/12/2023.
+//
+
+import UIKit
+
+class JournalEntryDetailViewcontroller: UITableViewController {
+
+    // MARK: - Properties
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var bodyTextView: UITextView!
+    @IBOutlet var photoImageView: UIImageView!
+}
