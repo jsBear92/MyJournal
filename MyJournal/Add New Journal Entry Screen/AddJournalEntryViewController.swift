cd50f1e99b360c79096bd302da4c8e10746827c1
Jaeseong Jeong <js104427@gmail.com>
2023-12-02 16:18:59 +0800

    Added features exploring text field and text view delegate methods

--- MyJournal/Add New Journal Entry Screen/AddJournalEntryViewController.swift
+++ MyJournal/Add New Journal Entry Screen/AddJournalEntryViewController.swift
@@ -7,16 +7,20 @@
 
 import UIKit
 
-class AddJournalEntryViewController: UIViewController {
+class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
     
     // MARK: - Properties
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var bodyTextView: UITextView!
     @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var saveButton: UIBarButtonItem!
     var newJournalEntry: JournalEntry?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleTextField.delegate = self
+        bodyTextView.delegate = self
+        updateSaveButtonState()
 
         // Do any additional setup after loading the view.
     }
@@ -34,6 +38,40 @@ class AddJournalEntryViewController: UIViewController {
         let photo = photoImageView.image
         let rating = 3
         newJournalEntry = JournalEntry(rating: rating, title: title, body: body, photo: photo)
+    }
+    
+    // MARK: - UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        saveButton.isEnabled = false
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateSaveButtonState()
+    }
+    
+    // MARK: - UITextViewDelegate
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            textView.resignFirstResponder()
+            return true
+        }
+        return true
+    }
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        saveButton.isEnabled = false
+    }
+    func textViewDidEndEditing(_ textView: UITextView) {
+        updateSaveButtonState()
+    }
+    
+    // MARK: - Private methods
+    private func updateSaveButtonState() {
+        let textFieldText = titleTextField.text ?? ""
+        let textViewText = bodyTextView.text ?? ""
+        saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty
     }
 
 }
