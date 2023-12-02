0cdb82a75bfe5d83a00b1915dfd401ca8c03ff66
Jaeseong Jeong <js104427@gmail.com>
2023-12-02 20:34:16 +0800

    added displaying a map snapshot on the journal detail

--- MyJournal/Journal Entry Detail Screen/JournalEntryDetailViewController.swift
+++ MyJournal/Journal Entry Detail Screen/JournalEntryDetailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 class JournalEntryDetailViewController: UITableViewController {
 
@@ -14,6 +15,7 @@ class JournalEntryDetailViewController: UITableViewController {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var bodyTextView: UITextView!
     @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var mapImageView: UIImageView!
     var sampleJournalEntryData = SampleJournalEntryData()
     var selectedJournalEntry: JournalEntry?
     
@@ -25,6 +27,28 @@ class JournalEntryDetailViewController: UITableViewController {
         titleLabel.text = selectedJournalEntry?.entryTitle
         bodyTextView.text = selectedJournalEntry?.entryBody
         photoImageView.image = selectedJournalEntry?.photo
+        getMapSnapshot()
+    }
+    
+    // MARK: - Private methods
+    private func getMapSnapshot() {
+        if let lat = selectedJournalEntry?.latitude, let long = selectedJournalEntry?.longitude {
+            let options = MKMapSnapshotter.Options()
+            options.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: long), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            options.size = CGSize(width: 300, height: 300)
+            options.preferredConfiguration = MKStandardMapConfiguration()
+            let snapShotter = MKMapSnapshotter(options: options)
+            snapShotter.start {
+                snapShot, error in
+                if let snapShot = snapShot {
+                    self.mapImageView.image = snapShot.image
+                } else if let error = error {
+                    print("snapShot error: \(error.localizedDescription)")
+                }
+            }
+        } else {
+            self.mapImageView.image = nil
+        }
     }
     
     // MARK: - Navigation
