6b0cd683c049fb066f4490d469b4610113637f4f
Jaeseong Jeong <js104427@gmail.com>
2023-12-02 18:24:41 +0800

    added location manager and map feature

--- MyJournal/Add New Journal Entry Screen/AddJournalEntryViewController.swift
+++ MyJournal/Add New Journal Entry Screen/AddJournalEntryViewController.swift
@@ -6,21 +6,28 @@
 //
 
 import UIKit
+import CoreLocation
 
-class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
+class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate, CLLocationManagerDelegate {
     
     // MARK: - Properties
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var bodyTextView: UITextView!
     @IBOutlet var photoImageView: UIImageView!
     @IBOutlet var saveButton: UIBarButtonItem!
+    @IBOutlet var getLocationSwitch: UISwitch!
+    @IBOutlet var getLocationSwitchLabel: UILabel!
     var newJournalEntry: JournalEntry?
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         titleTextField.delegate = self
         bodyTextView.delegate = self
         updateSaveButtonState()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
 
         // Do any additional setup after loading the view.
     }
@@ -37,7 +44,9 @@ class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITe
         let body = bodyTextView.text ?? ""
         let photo = photoImageView.image
         let rating = 3
-        newJournalEntry = JournalEntry(rating: rating, title: title, body: body, photo: photo)
+        let lat = currentLocation?.coordinate.latitude
+        let long = currentLocation?.coordinate.longitude
+        newJournalEntry = JournalEntry(rating: rating, title: title, body: body, photo: photo, latitude: lat, longitude: long)
     }
     
     // MARK: - UITextFieldDelegate
@@ -67,11 +76,37 @@ class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITe
         updateSaveButtonState()
     }
     
+    // MARK: - Actions
+    @IBAction func getLocationSwitchValueChanged(_ sender: UISwitch) {
+        if getLocationSwitch.isOn {
+            getLocationSwitchLabel.text = "Getting Location..."
+            locationManager.requestLocation()
+        } else {
+            currentLocation = nil
+            getLocationSwitchLabel.text = "Get location"
+        }
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let myCurrentLocation = locations.first {
+            currentLocation = myCurrentLocation
+            getLocationSwitchLabel.text = "Done"
+            updateSaveButtonState()
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+    
     // MARK: - Private methods
     private func updateSaveButtonState() {
         let textFieldText = titleTextField.text ?? ""
         let textViewText = bodyTextView.text ?? ""
-        saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty
+        if getLocationSwitch.isOn {
+            saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty && !(currentLocation == nil)
+        } else {
+            saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty
+        }
     }
-
 }
